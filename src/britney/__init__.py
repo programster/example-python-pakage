9995cda615c02af7d8f0aa091ed5f5955bf07a19
Programster <sdpagent@gmail.com>
2021-10-04 16:30:21 +0100

    fixes package now that the data files are within a subfolder called "data"

--- src/britney/__init__.py
+++ src/britney/__init__.py
@@ -4,6 +4,6 @@ def run(name=None):
     """
     Places binary image file wherever you called this script from.
     """
-    source=os.path.dirname(__file__) + "/britney.jpg"
+    source=os.path.dirname(__file__) + "/data/britney.jpg"
     dest=os.getcwd() + "/britney.jpg"
     with open(source, 'rb') as src, open(dest, 'wb') as dst: dst.write(src.read())
